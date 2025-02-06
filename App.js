de0bcdccb43789098b8defd3b89691f0f3ec9001
luttinenjoni <luttinenjoni@gmail.com>
2025-02-06 17:06:16 +0200

    Added distance, duration and sports type to flatlist

--- App.js
+++ App.js
@@ -1,40 +1,61 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
-import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import { FlatList, SafeAreaView, Text, View, Button } from 'react-native';
 import { Dropdown } from 'react-native-paper-dropdown';
 import { Provider as PaperProvider } from 'react-native-paper';
-import {Pstyles} from './styles/Styles.js'
+import Pstyles from './styles/Styles.js'
+import InputSpinner from "react-native-input-spinner";
 
 const STYPES = [
-  { label: 'Ball games', value: 'bgames' },
-  { label: 'Gym', value: 'gym' },
+  { label: 'Jogging', value: 'Jogging' },
+  { label: 'Swimming', value: 'Swimming' },
+  { label: 'Cycling', value: 'Cycling' }
 ]
 
 export default function App() {
   const [stype, setStype] = useState();
+  const [inputInfo, SetInputInfo] = useState([])
+  const [d, setD] = useState(0);
+  const [duration, setDuration] = useState(0)
+  
+
+  function addSportsType() {
+    SetInputInfo([...inputInfo ,'Sports type: ' + stype,'Distance: ' + d + ' km', 'Duration: ' + duration + ' minutes'])
+  }
 
   return (
     <PaperProvider>
-      <SafeAreaView style={styles.container}>
-        <Dropdown
+      <SafeAreaView style={Pstyles.container}>
+        <Dropdown style={Pstyles.inputfield}
           placeholder='Select sport type'
           label='sport type'
           options={STYPES}
           value={stype}
           onSelect={setStype}
         />
-        <StatusBar style="auto" />
+        <Text style={Pstyles.InputSpinnerHeader} >Distance</Text>
+        <InputSpinner
+          value={d}
+          onChange={setD}
+        />
+        <Text style={Pstyles.InputSpinnerHeader} >Duration</Text>
+         <InputSpinner
+          value={duration}
+          onChange={setDuration}
+        />
+        
+        <Button title='Add'
+          
+          onPress={addSportsType} 
+        />
+
+        <FlatList style={Pstyles.flatlist}
+          data={inputInfo}
+          renderItem={({item}) => <Text>{item}</Text>}
+        
+      />
       </SafeAreaView>
+      
     </PaperProvider>
-    
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ styles/Styles.js
@@ -0,0 +1,21 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    justifyContent: 'center',
+    
+  },
+  flatlist: {
+    borderWidth: 2,
+    padding: 10,
+    margin: 50,
+    marginTop: 300
+  },
+  InputSpinnerHeader: {
+    textAlign: 'center',
+    fontWeight: 'bold',
+    marginTop: 10
+  }
+});
